66879176c8ef479dc601c8c04109b77cc26ab4d3
Александр Рутковский <alur.rutkov@gmail.com>
2024-03-05 18:11:25 +0300

    Добавил:  - логирование в Loki

---
+++ src/main/kotlin/ru/pmlite/api/config/OpenTelemetryConfig.kt
@@ -0,0 +1,49 @@
+package ru.pmlite.api.config
+
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.api.common.Attributes
+import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender
+import io.opentelemetry.sdk.logs.LogRecordProcessor
+import io.opentelemetry.sdk.logs.SdkLoggerProvider
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor
+import io.opentelemetry.sdk.resources.Resource
+import io.opentelemetry.semconv.ResourceAttributes
+import org.springframework.beans.factory.ObjectProvider
+import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+
+
+@Configuration
+class OpenTelemetryConfig  {
+
+  @Bean
+  fun installOpenTelemetryAppender(openTelemetry: OpenTelemetry): ApplicationRunner {
+    return ApplicationRunner { OpenTelemetryAppender.install(openTelemetry) }
+  }
+
+
+
+  @Bean
+  fun otelSdkLoggerProvider(
+    environment: Environment,
+    logRecordProcessors: ObjectProvider<LogRecordProcessor>
+  ): SdkLoggerProvider {
+    val applicationName = environment.getProperty("spring.application.name", "application")
+    val springResource = Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, applicationName))
+    val builder = SdkLoggerProvider.builder().setResource(Resource.getDefault().merge(springResource))
+    logRecordProcessors.orderedStream().forEach(builder::addLogRecordProcessor)
+    return builder.build()
+  }
+
+  @Bean
+  fun otelLogRecordProcessor(): LogRecordProcessor {
+    return BatchLogRecordProcessor
+      .builder(OtlpGrpcLogRecordExporter.builder().setEndpoint("http://192.168.178.101:4317").build())
+      .build()
+  }
+
+
+}
